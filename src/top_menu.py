cf4a8bb9130908975a13d9f50c86586801fae6cd
b-maskell <112417820+b-maskell@users.noreply.github.com>
2023-07-10 00:29:29 +0100

    added basic menu

--- src/top_menu.py
+++ src/top_menu.py
@@ -1,8 +1,27 @@
-#import rock-paper-scissors
+import rock_paper_scissors
 
+def top_menu():
+    """Print top menu listing available games to play"""
 
-print(
+    print(
 """Games List
 (1) Number Guess Game
 (2) Rock Paper Scissors
 """)
+
+    while True:
+        user_choice = int(input("Enter Choice (1-2): "))                      
+        if user_choice < 1 or user_choice > 2:              
+            print("Bad Choice - pick again between 1 and 2")
+        else:
+            break
+
+    if user_choice == 1:
+        pass
+    elif user_choice == 2:
+        rock_paper_scissors.run_game()
+
+
+ # Main - code to call menu
+if __name__ == "__main__":
+    top_menu()
